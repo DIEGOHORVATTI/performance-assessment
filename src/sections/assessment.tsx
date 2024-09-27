1f53f9b83218de0816efcecd4f7b5c852494008c
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-09-27 19:02:10 -0300

    Refactor assessment section and add dynamic questions rendering

--- src/sections/assessment.tsx
+++ src/sections/assessment.tsx
@@ -1,12 +1,53 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Divider, Stack, Typography } from '@mui/material'
 import { Logo } from '../components/logo'
 
+type Question = {
+  id: number
+  question: string
+  description: string
+}
+
 export default function Assessment() {
-  const mockQuestions = [
-    'Você se sente confortável com o seu ambiente de trabalho?',
-    'Você se sente confortável com a sua equipe de trabalho?',
-    'Você se sente confortável com a sua carga de trabalho?',
+  const mockQuestions: Array<Question> = [
+    {
+      id: 1,
+      question: 'Como Avalia suas Entregas?',
+      description: 'Avalie a qualidade e a quantidade de entregas realizadas.',
+    },
+    {
+      id: 2,
+      question: 'Como Avalia sua Motivação?',
+      description: 'Avalie o nível de motivação e engajamento com o trabalho.',
+    },
+    {
+      id: 3,
+      question: 'Como Avalia sua Resiliência?',
+      description: 'Avalie a capacidade de lidar com adversidades e superar desafios.',
+    },
   ]
+
+  const RenderQuestion = ({ id, question, description }: Question) => (
+    <Stack
+      direction="row"
+      spacing={1}
+      alignItems="flex-start"
+      divider={
+        <Divider orientation="vertical" sx={{ backgroundColor: '#047835', width: 2, height: 50 }} />
+      }
+    >
+      <Typography variant="h6" color="#047835" fontWeight="bold">
+        {id}
+      </Typography>
+
+      <Stack direction="column" spacing={1}>
+        <Typography variant="body1" color="#047835" fontWeight="bold">
+          {question}
+        </Typography>
+
+        <Typography variant="body2">{description}</Typography>
+      </Stack>
+    </Stack>
+  )
 
   return (
     <Stack direction="column">
@@ -15,18 +56,16 @@ export default function Assessment() {
         alignItems="center"
         sx={{ backgroundColor: '#e3e9db', borderBottom: '1px solid #047835', p: 1 }}
       >
-        <Logo width={30} height={30} />
+        <Logo width={25} height={25} />
 
-        <Typography variant="h6" color="#047835" fontWeight="bold">
+        <Typography variant="subtitle2" color="#047835" fontWeight="bold">
           Avaliação de Performace
         </Typography>
       </Stack>
 
       <Stack direction="column" spacing={2}>
-        {mockQuestions.map((question, index) => (
-          <Box key={index} sx={{ backgroundColor: 'white', padding: 2 }}>
-            <Typography variant="body1">{question}</Typography>
-          </Box>
+        {mockQuestions.map(({ question, description, id }, index) => (
+          <RenderQuestion key={index} {...{ id, question, description }} />
         ))}
       </Stack>
     </Stack>
