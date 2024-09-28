0fe420f2e28608ece5b953da5a7d9e4be72b9705
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-09-27 23:06:31 -0300

    Refactor SliderCustom component to use custom styled Slider and fix rendering issue with thumb

---
+++ src/sections/assessment/components/SliderCustom.tsx
@@ -0,0 +1,49 @@
+import { Slider } from '@mui/material'
+import { styled } from '@mui/system'
+
+/**
+ * @title Nota sobre a Customização
+ * @description o componente SliderCustom só existe pois a entrada do thumb está com problemas de renderização
+ *
+ *  slotProps={{
+ *   thumb: {
+ *    children: <Logo />,
+ *   },
+ *  }}
+ *
+ * Isso acima deveria funcionar mas não está funcionando, então foi necessário criar um componente customizado
+ * @todo Quando uma nova versão do MUI for lançada, remover o SliderCustom e usar slotProps thumb do Slider
+ */
+export const SliderCustom = styled(Slider)<{ fill: string }>(({ fill }) => ({
+  color: 'white',
+  height: 9,
+  '& .MuiSlider-track': {
+    color: fill,
+  },
+  // Hide first and last marker
+  '& .MuiSlider-mark[data-index="0"], & .MuiSlider-mark[data-index="4"]': {
+    display: 'none',
+  },
+  // Maker divider
+  '& .MuiSlider-mark': {
+    backgroundColor: '#808080',
+    height: 15,
+  },
+  '& .MuiSlider-thumb': {
+    color: '#1dc25a',
+    // Marker Icon Suzano
+    '&:before': {
+      content: '""',
+      position: 'absolute',
+      top: -10,
+      right: -50,
+      width: 40,
+      height: 50,
+      backgroundImage: 'url("/suzano.svg")',
+      transform: 'translate(-50%, -50%)',
+      boxShadow: 'none',
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+    },
+  },
+}))
--- src/sections/assessment/components/SliderRequest.tsx
+++ src/sections/assessment/components/SliderRequest.tsx
@@ -1,5 +1,5 @@
-import { Divider, Slider, Stack } from '@mui/material'
-import { styled } from '@mui/system'
+import { Divider, Stack } from '@mui/material'
+import { SliderCustom } from './SliderCustom'
 
 import { useRequestSWR } from '@/hooks/use-request'
 
@@ -28,50 +28,3 @@ export const SliderRequest = ({ color }: Props) => {
     </Stack>
   )
 }
-
-// TODO: Quando uma nova versão do MUI for lançada, remover o SliderCustom e usar slotProps thumb do Slider
-/**
- * @title Nota da Customização
- * @description o componente SliderCustom só existe pois a entrada do thumb está com problemas de renderização
- *
- *  slotProps={{
- *   thumb: {
- *    children: <Logo />,
- *   },
- *  }}
- *
- * Isso acima deveria funcionar mas não está funcionando, então foi necessário criar um componente customizado
- */
-const SliderCustom = styled(Slider)<{ fill: string }>(({ fill }) => ({
-  color: 'white',
-  height: 9,
-  '& .MuiSlider-track': {
-    color: fill,
-  },
-  // Hide first and last marker
-  '& .MuiSlider-mark[data-index="0"], & .MuiSlider-mark[data-index="4"]': {
-    display: 'none',
-  },
-  // Maker divider
-  '& .MuiSlider-mark': {
-    backgroundColor: '#808080',
-    height: 15,
-  },
-  '& .MuiSlider-thumb': {
-    color: '#1dc25a',
-    // Marker Icon Suzano
-    '&:before': {
-      content: '""',
-      position: 'absolute',
-      top: -10,
-      right: -50,
-      width: 40,
-      height: 50,
-      backgroundImage: 'url("/suzano.svg")',
-      transform: 'translate(-50%, -50%)',
-      boxShadow: 'none',
-      backgroundSize: 'contain',
-      backgroundRepeat: 'no-repeat',
-    },
-  },
-}))
